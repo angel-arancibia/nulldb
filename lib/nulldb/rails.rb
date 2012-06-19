40bfc4a65284df4d8abd9d920daf56713f56595d
Angel <angel.arancibia@gmail.com>
2012-06-19 19:26:29 -0300

    fix for Padrino framework

--- lib/nulldb/rails.rb
+++ lib/nulldb/rails.rb
@@ -1,4 +1,12 @@
 require 'nulldb/core'
 
 # Need to defer calling Rails.root because when bundler loads, Rails.root is nil
-NullDB.configure {|ndb| def ndb.project_root;Padrino.root;end}
+
+# Ugly quick fix ..
+root = nil
+root = Rails.root unless defined?(Rails).nil?
+root = Padrino.root unless defined?(Padrino).nil?
+
+raise Exception, "Project root not defined" if root.nil?
+
+NullDB.configure {|ndb| def ndb.project_root;root;end}
