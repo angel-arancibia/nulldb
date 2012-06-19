45773d57e05099c4c46a0ec238c8f1b90847e472
Angel <angel.arancibia@gmail.com>
2012-06-19 19:41:21 -0300

    shit ...

--- lib/nulldb/framework.rb
+++ lib/nulldb/framework.rb
@@ -1,12 +1,5 @@
 require 'nulldb/core'
 
 # Need to defer calling Rails.root because when bundler loads, Rails.root is nil
-
 # Ugly quick fix ..
-root = nil
-root = Rails.root unless defined?(Rails).nil?
-root = Padrino.root unless defined?(Padrino).nil?
-
-raise Exception, "Project root not defined" if root.nil?
-
-NullDB.configure {|ndb| def ndb.project_root;root;end}
+NullDB.configure {|ndb| def ndb.project_root; (not defined?(Padrino).nil? )? Padrino.root : Rails.root ;end}
